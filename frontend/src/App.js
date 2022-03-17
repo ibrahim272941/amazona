d5591024012ae2cffae8d6f8ca7a914d26a72695
Ibrahim Coban <ibrahimcoban272941@gmail.com>
2022-03-17 23:02:17 +0100

    main Page is created

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import data from './data';
 
 function App() {
   return (
@@ -6,7 +7,28 @@ function App() {
       <header>
         <a href="/">Amazon</a>
       </header>
-      <main>List Product</main>
+      <main>
+        <h1>Featured Products</h1>
+        <div className="products">
+          {data.products.map((item, i) => (
+            <div className="product" key={item.slug}>
+              <a href={`/product/${item.slug}`}>
+                <img src={item.image} alt="" />
+              </a>
+              <div className="product-info">
+                <p>{item.name}</p>
+                <a href={`/product/${item.slug}`}>
+                  <p>
+                    {' '}
+                    <strong>{item.price}€</strong>
+                  </p>
+                </a>
+                <button>Add Chart</button>
+              </div>
+            </div>
+          ))}
+        </div>
+      </main>
     </div>
   );
 }
