adffba3e72429b96507d1f4246dbd62aee93c84e
Ibrahim Coban <ibrahimcoban272941@gmail.com>
2022-03-17 22:30:39 +0100

    Mern fullstack amazon clone

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">Amazon</a>
+      </header>
+      <main>List Product</main>
+    </div>
+  );
+}
+
+export default App;
