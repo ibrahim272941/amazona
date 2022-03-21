447c1843feae3ce01d5a03048393d18b06804dbd
Ibrahim Coban <ibrahimcoban272941@gmail.com>
2022-03-21 22:58:04 +0100

    bootstrap is added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,35 @@
 import './App.css';
-import data from './data';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomeScreen from './screen/HomeScreen';
 import ProductScreen from './screen/ProductScreen';
+import { Container, Navbar } from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap';
 
 function App() {
   return (
     <Router>
-      <div>
+      <div className="d-flex flex-column site-contianer">
         <header>
-          <Link to="/">Amazon</Link>
+          <Navbar bg="dark" variant="dark">
+            <Container>
+              <LinkContainer to="/">
+                <Navbar.Brand>Amazon</Navbar.Brand>
+              </LinkContainer>
+            </Container>
+          </Navbar>
         </header>
         <main>
-          <Routes>
-            <Route path="/product/:slug" element={<ProductScreen />} />
-            <Route path="/" element={<HomeScreen />} />
-          </Routes>
+          <Container>
+            <Routes>
+              <Route path="/product/:slug" element={<ProductScreen />} />
+              <Route path="/" element={<HomeScreen />} />
+            </Routes>
+          </Container>
         </main>
+        <footer>
+          <div className="text-center">All rights deserved</div>
+        </footer>
       </div>
     </Router>
   );
